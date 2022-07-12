24e5ca4575e901edb078703c8711489e67486940
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-07-12 14:25:07 -0600

    heroku post /signals

--- client.py
+++ client.py
@@ -4,24 +4,24 @@ import time
 
 ser = serial.Serial('COM4', 9600, timeout=1)
 
-h = {'Content-type': 'application/json'}  
-
 while True:    
 
     raw_data = ser.readline()
     if raw_data:
-    
-        sen=str(raw_data).split("'")
-        sen = sen[1].split('\\')
-        s = sen[0]
-        data = {
-            'stop_id': 1,
-            'bus_name': 'Dale Up',
-            'bus_id': s
-        }
-        req = requests.post("https://track-bus-nevsoft.herokuapp.com/signals", json=data)
-        
-        value = data['bus_id']
-        print(f"{req.text} tarjeta: {value}") 
-        
+        try:
+            sen=str(raw_data).split("'")
+            sen = sen[1].split('\\')
+            s = sen[0]
+            data = {
+                'stop_id': 1,
+                'bus_name': 'Dale Up',
+                'bus_id': s
+            }
+            req = requests.post("https://track-bus-nevsoft.herokuapp.com/signals", json=data)
+            
+            value = data['bus_id']
+            print(f"{req.text} tarjeta: {value}") 
+        except Exception as e:
+            print(e)
+
     time.sleep(1)
