a12df86205f538822e7914290ad8d9da09ddfebc
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-08-01 14:26:41 -0600

    /rf

--- client.py
+++ client.py
@@ -3,7 +3,13 @@ import requests
 import time
 
 ser = serial.Serial('COM4', 9600, timeout=1)
+get = requests.get("https://track-bus-nevsoft.herokuapp.com/routes", headers={
+"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+})
 
+routes = get.json()
+#print(route)
+#print(routes[0])
 while True:    
 
     raw_data = ser.readline()
@@ -12,11 +18,13 @@ while True:
             sen=str(raw_data).split("'")
             sen = sen[1].split('\\')
             s = sen[0]
-            data = {
-                'stop_id': 1,
-                'bus_name': 'Dale Up',
-                'bus_id': s
-            }
+            for i in routes:
+                if i['bus_id'] == s:
+                    data = {
+                        'stop_id': 1,
+                        'bus_name': i['bus_name'],
+                        'bus_id': s
+                    }
             req = requests.post("https://track-bus-nevsoft.herokuapp.com/signals", json=data)
             
             value = data['bus_id']
